c41cfedbc7a612748baa8673d29c1e33d0325a89
Lexi Knight <lexi.knight@mail.utoronto.ca>
2024-11-17 18:04:00 -0800

    downloading and saving raw data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,39 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the climate data from opendatatoronto.
+# Author: Lexi Knight
+# Date: 17 November 2024
+# Contact: lexi.knight@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: none
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+# Install the necessary packages if needed
+# install.packages(c("httr", "readxl", "tidyverse", "data.table"))
+
+# Load required libraries
+library(httr)
+library(readxl)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(data.table)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# URL to the CSV file
+csv_url <- "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/77e593cf-877b-4bcc-bfdf-0b99e2935e5f/resource/cd92b939-f618-49db-b497-b6daf06ecf02/download/Raw%20data.csv"
 
+# Download the file to a temporary file using httr::GET
+response <- GET(csv_url, write_disk(tf <- tempfile(fileext = ".csv")))
 
+# Check if the response is successful (HTTP 200)
+if (response$status_code != 200) {
+  stop("Failed to download file: HTTP ", response$status_code)
+}
+
+# Read the CSV file using fread
+raw_data <- fread(tf)
+
+# View the first few rows to confirm the data
+head(raw_data)
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(raw_data, "data/01-raw_data/climate_challenges_raw_data.csv")
